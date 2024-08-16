11b62ac1eaa3296ae370946bdd7118cc12ebbf9f
Luciano Tomás Quevedo <104854482+Tomix00@users.noreply.github.com>
2024-08-16 21:38:48 +0000

    finished and added ej5

--- Practico 0/practico0.sh
+++ Practico 0/practico0.sh
@@ -12,7 +12,7 @@ titulo(){
     sleep 3
     clear
 
-    for ((i=3 ; i>0 ; --i))
+    for ((i=5 ; i>0 ; --i))
     do
         echo "ej:      $EJ"
         echo "info:    $INFO"
@@ -26,19 +26,19 @@ titulo(){
 
 titulo "1" "Muestra las lineas donde matchea lo indicado - 'model name'" "grep 'model name' /proc/cpuinfo"
 grep 'model name' /proc/cpuinfo
-sleep 5
+sleep 7
 
 titulo "2" "Cuenta cuantas lineas contiene el archivo" "grep 'model name' /proc/cpuinfo | wc -l"
 echo -e "grep 'model name' /proc/cpuinfo - solo para visualizar que se esta contando:\n"
 grep 'model name' /proc/cpuinfo
 echo -e "\n"
 grep 'model name' /proc/cpuinfo | wc -l
-sleep 5
+sleep 7
 
 titulo "3" "Recorta una columna especifica, elimina los espacios en blanco y los guarda en un archivo nombes.csv" "cut -d';' -f2 heroes.csv | grep . >> nombres.csv "
 cut -d';' -f2 heroes.csv | grep . >> nombres.csv
 echo "Chequee el archivo nombres.csv en su directorio"
-sleep 5
+sleep 7
 
 titulo "4" "Obtener fecha de min(minimas) y max(maximas) de temperaturas" "sort -k5 -n -t' ' weather_cordoba.in | ... | cut -d' ' -f1-3"
 echo "fecha de temperatura maxima: "
@@ -49,14 +49,12 @@ echo -e "\n"
 echo "fecha de temperatura minima: "
 echo "comando: sort -k5 -n -t' ' weather_cordoba.in | head -n 1 | cut -d' ' -f1-3"
 sort -k5 -n -t' ' weather_cordoba.in | head -n 1 | cut -d' ' -f1-3
-sleep 5
+sleep 7
 
-
-# fecha de maxima de maximas
-# sort -k5 -n -t' ' weather_cordoba.in | tail -n 1 | cut -d' ' -f1-3
-# fecha de minima de minimas
-# sort -k5 -n -t' ' weather_cordoba.in | head -n 1 | cut -d' ' -f1-3
-
+titulo "5" "Ordenar por ranking atpplayers.in" "sort -k3 -n -t' ' >> atpplayers_ranked.txt"
+sort -k3 -n -t' ' atpplayers.in >> atpplayers_ranked.txt
+echo "Chequee el archivo atpplayers_ranked.txt en su directorio"
+sleep 7
 
 clear
 echo "end of script"
