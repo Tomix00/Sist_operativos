65086e8d22ff27f1010cce888de6bfcdc303b20b
Luciano Tomás Quevedo <104854482+Tomix00@users.noreply.github.com>
2024-08-19 23:24:26 -0300

    finished practico0

--- Practico 0/practico0.sh
+++ Practico 0/practico0.sh
@@ -24,6 +24,11 @@ titulo(){
     done
 }
 
+if test -f nombres.csv; then rm nombres.csv; fi
+if test -f atpplayers_ranked.out; then rm atpplayers_ranked.out; fi
+if test -d serie; then rm -r serie; fi
+
+
 titulo "1" "Muestra las lineas donde matchea lo indicado - 'model name'" "grep 'model name' /proc/cpuinfo"
 grep 'model name' /proc/cpuinfo
 sleep 7
@@ -36,7 +41,7 @@ grep 'model name' /proc/cpuinfo | wc -l
 sleep 7
 
 titulo "3" "Recorta una columna especifica, elimina los espacios en blanco y los guarda en un archivo nombes.csv" "cut -d';' -f2 heroes.csv | grep . >> nombres.csv "
-#cut -d';' -f2 heroes.csv | grep . >> nombres.csv
+cut -d';' -f2 heroes.csv | grep . >> nombres.csv
 echo "Chequee el archivo nombres.csv en su directorio"
 sleep 7
 
@@ -52,9 +57,27 @@ sort -k5 -n -t' ' weather_cordoba.in | head -n 1 | cut -d' ' -f1-3
 sleep 7
 
 titulo "5" "Ordenar por ranking atpplayers.in" "sort -k3 -n -t' ' >> atpplayers_ranked.txt"
-#sort -k3 -n -t' ' atpplayers.in >> atpplayers_ranked.txt
+sort -k3 -n -t' ' atpplayers.in >> atpplayers_ranked.out
 echo "Chequee el archivo atpplayers_ranked.txt en su directorio"
 sleep 7
+
+titulo "6" "Ordenar por puntos y desempatar por diferencia" "awk '{printf '%-15s|Ptos: %-4s|Dif: %s \n',1,2,7-8}' superliga.in | sort -t'|' -k2 -r -k3"
+awk '{printf "%-15s|Ptos: %-4s|Dif: %s \n",$1,$2,$7-$8}' superliga.in | sort -t'|' -k2 -r -k3
+sleep 7
+
+titulo "7" "MAC address del equipo" "ip addr | grep 'link/ether'"
+ip addr | grep "link/ether"
+sleep 7
+
+titulo "8" "Renombrar multiples archivos en una carpeta" "for f in serie/*srt; do mv -- '$f' '${f%_es.srt}.srt' ; done"
+mkdir serie
+touch serie/fma_S01E{1..10}_es.srt
+ls serie
+echo -e "\n"
+for f in serie/*srt; do mv -- "$f" "${f%_es.srt}.srt"; done
+ls serie
+sleep 7
+
 
 
 clear
